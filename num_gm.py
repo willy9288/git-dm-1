60860d8ca0d6b4171ad04388f140e85cde4302f2
wy <wy@gmail.com>
2024-07-13 14:54:08 +0800

    新增猜的次數

--- num_gm.py
+++ num_gm.py
@@ -3,6 +3,7 @@ import random
 x = random.randint(1, 50)
 # print(x)
 
+
 for i in range(5):
 
     y = int(input("請猜一個數字:"))
@@ -16,5 +17,9 @@ for i in range(5):
             print("猜高一點!")
         else:
             print("猜低一點~~~")
+
 if x != y:
     print(f"答案為:{x}")
+
+else:
+    print(f"猜了:{i+1}次")
