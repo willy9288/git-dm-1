93bb75b4ca2d831d5c1a9185e51b1e46b066ee67
wy <wy@gmail.com>
2024-07-13 15:57:50 +0800

    回複

---
+++ num_gm.py
@@ -0,0 +1,25 @@
+import random
+
+x = random.randint(1, 50)
+# print(x)
+
+
+for i in range(15):
+
+    y = int(input("請猜一個數字:"))
+
+    if x == y:
+        print("猜對了!!!")
+        break
+
+    else:
+        if x > y:
+            print("猜高一點!")
+        else:
+            print("猜低一點~~~")
+
+if x != y:
+    print(f"答案為:{x}")
+
+else:
+    print(f"猜了:{i+1}次")
