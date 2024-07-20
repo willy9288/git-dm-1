276f845f5065ff0d172a5f936cd8ffefff0c6bc2
wy <wy@gmail.com>
2024-07-20 10:31:51 +0800

    增加指令

--- num_gm.py
+++ num_gm.py
@@ -4,11 +4,7 @@ x = random.randint(1, 50)
 # print(x)
 
 
-<<<<<<< HEAD
 for i in range(15):
-=======
-for i in range(5):
->>>>>>> dev
 
     y = int(input("請猜一個數字:"))
 
@@ -28,8 +24,4 @@ if x != y:
 else:
     print(f"猜了:{i+1}次")
 
-<<<<<<< HEAD
-
-=======
->>>>>>> dev
 print()
